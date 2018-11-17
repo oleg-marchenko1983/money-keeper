7a8087b53febffd3aa760417addec6b07f195196
Oleg Marchenko <ynwa.om@gmail.com>
2018-11-17 22:28:45 +0200

    correction js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
-const money = +prompt('Ваш бюджет на месяц?', '');
-const time = prompt('Введите дату в формате YYYY-MM-DD', 'YYYY-MM-DD');
-const expensesRequired = +prompt('Введите обязательную статью расходов в этом месяце', '');
-const expensesHowMatch = +prompt('Во сколько обойдется?', '');
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-const appData = {
-  budget: money,
-  timeData: time,
-  expenses: {
-    expensesRequired: expensesHowMatch,
-  },
-  optionalExpenses: {},
-  income: [],
-  saving: false
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
 };
 
-const dayInMonth = 30;
-const result = (money - expensesRequired) / dayInMonth;
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
 
 console.log(appData.budget);
 console.log(appData.timeData);
 console.log(appData.expenses);
 console.log(appData.saving);
-
-alert(result);
